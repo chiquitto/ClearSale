97e37733cf7a69e4cd917ef822d4c6b998d6b5a5
Alisson Chiquitto <chiquitto@gmail.com>
2015-09-14 16:40:57 -0300

    Test 1 HTML

---
+++ testHtml/test1.php
@@ -0,0 +1,69 @@
+<html>
+  <body>
+    <form method="POST" target="iFrameStart"
+          action="http://homolog.clearsale.com.br/start/Entrada/EnviarPedido.aspx">
+      <input type="hidden" name="CodigoIntegracao" value="F971F710-4240-4307-93CF-E815CBC7B010" />
+      <input type="hidden" name="PedidoID" value="12345" />
+      <input type="hidden" name="Data" value="05/06/2010 14:10:00" />
+      <input type="hidden" name="IP" value="189.19.247.9" />
+      <input type="hidden" name="Total" value="288.80" />
+      <input type="hidden" name="TipoPagamento" value="1" />
+      <input type="hidden" name="Parcelas" value="6" />
+      <input type="hidden" name="Cartao_Bin" value="645896" />
+      <input type="hidden" name="Cartao_Fim" value="1234" />
+      <input type="hidden" name="Cartao_Numero_Mascarado"
+             value="645896******1234" />
+      <!--DADOS DA COMPRA -->
+      <input type="hidden" name="Cobranca_Nome" value="Teste" />
+      <input type="hidden" name="Cobranca_Nascimento" value="01/01/1980 00:00:00"
+             />
+      <input type="hidden" name="Cobranca_Email" value="teste@clearsale.com.br"
+             />
+      <input type="hidden" name="Cobranca_Documento" value="XXXXXXXXXXX" />
+      <input type="hidden" name="Cobranca_Logradouro" value="Rua 4" />
+      <input type="hidden" name="Cobranca_Logradouro_Numero" value="2" />
+      <input type="hidden" name="Cobranca_Logradouro_Complemento" value="Casa" />
+      <input type="hidden" name="Cobranca_Bairro" value="Teste Bairro" />
+      <input type="hidden" name="Cobranca_Cidade" value="Teste Cidade" />
+      <input type="hidden" name="Cobranca_Estado" value="SP" />
+      <input type="hidden" name="Cobranca_CEP" value="XXXXXXXX" />
+      <input type="hidden" name="Cobranca_Pais" value="Bra" />
+      <input type="hidden" name="Cobranca_DDD_Telefone_1" value="11" />
+      <input type="hidden" name="Cobranca_Telefone_1" value="12345678" />
+      <input type="hidden" name="Cobranca_DDD_Celular" value="11" />
+      <input type="hidden" name="Cobranca_Celular" value="999999999" />
+      <!--ENTREGA -->
+      <input type="hidden" name="Entrega_Nome" value="Teste" />
+      <input type="hidden" name="Entrega_Nascimento" value="01/01/1980 00:00:00"
+             />
+      <input type="hidden" name="Entrega_Email" value="teste@clearsale.com.br" />
+      <input type="hidden" name="Entrega_Documento" value="XXXXXXXXXXX" />
+      <input type="hidden" name="Entrega_Logradouro" value="Rua doTeste" />
+      <input type="hidden" name="Entrega_Logradouro_Numero" value="2" />
+      <input type="hidden" name="Entrega_Logradouro_Complemento" value="Casa" />
+      <input type="hidden" name="Entrega_Bairro" value="Teste Bairro" />
+      <input type="hidden" name="Entrega_Cidade" value="Teste Cidade" />
+      <input type="hidden" name="Entrega_Estado" value="SP" />
+      <input type="hidden" name="Entrega_CEP" value="XXXXXXXX" />
+      <input type="hidden" name="Entrega_Pais" value="Bra" />
+      <input type="hidden" name="Entrega_DDD_Telefone_1" value="00" />
+      <input type="hidden" name="Entrega_Telefone_1" value="12345678" />
+
+      <input type="hidden" name="Entrega_DDD_Celular" value="11" />
+      <input type="hidden" name="Entrega_Celular" value="999999999" />
+      <input type="hidden" name="Item_ID_1" value="12345" />
+      <input type="hidden" name="Item_Nome_1" value="Teste Produto)" />
+      <input type="hidden" name="Item_Qtd_1" value="1" />
+      <input type="hidden" name="Item_Valor_1" value="750.00" />
+      <input type="hidden" name="Item_Categoria_1" value="Teste" />
+      <input type="submit" value="Enviar" />
+
+    </form>
+
+    <iframe id="iFrameStart" name="iFrameStart"
+            src="">
+    <p>Seu Browser não suporta iframes</p>
+    </iframe>
+
+  </body>
+</html>
